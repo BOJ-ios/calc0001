273d2b0058dd857b1c7cd5fce06c683bc54334e6
sejin-chun <101098015+sejin-chun@users.noreply.github.com>
2022-05-04 08:20:11 +0900

    Create setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ setuptools.setup (
   description='oss-dev my calculator 0001',
   author='sejinchun',
   author_email='sjchun@dau.ac.kr',
+  url = "https://github.com/sejin-chun/calc0001",
+  download_url = "https://github.com/sejin-chun/calc0001/archive/refs/tags/v0.0.1.zip",
   install_requires=['pytest'],
   long_description = 'oss-dev calculator python module',
   long_description_content_type = 'text/markdown',
   classifiers=[
-      "Programming Language :: Python :: 3",
+      "Programmming Language :: Python :: 3",
       "Operating System :: OS Independent",
   ],
 )
